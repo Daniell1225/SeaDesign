6ee77fdc15157fdfdf879f6dd1f29770da059d38
Daniell1225 <trooperofficiall@gmail.com>
2026-08-07 08:55:46 +0300

    app.js

--- app.js
+++ app.js
@@ -243,7 +243,7 @@ document.getElementById("sendOrderBtn").addEventListener("click", function () {
         sum + parseFloat(item.price) * item.quantity
     ,0);
 
-    emailjs.send(
+emailjs.send(
     "service_w97mo77",
     "template_tv00i1d",
     {
